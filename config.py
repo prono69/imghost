edc5ac6aa1cb83b01582a67e95b29ce855e8f470
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-19 10:36:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ load_dotenv()  # Load environment variables from a .env file
 API_ID = os.getenv('API_ID', 'your_api_id')  
 API_HASH = os.getenv('API_HASH', 'your_api_hash')  
 BOT_TOKEN = os.getenv('BOT_TOKEN', 'your_bot_token')  
-MONGODB_URL = os.getenv('MONGODB_URL', 'your_mongodb_url')  # MongoDB URL
+MONGODB_URI = os.getenv('MONGODB_URL', 'your_mongodb_url')  # MongoDB URL
 BOT_IMAGE_URL = os.getenv('BOT_IMAGE_URL', 'your_default_image_url')  # Add this line
