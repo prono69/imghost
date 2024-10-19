0a9c44b50d2d2a0de9d40865d8688abeab4f5964
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-19 10:34:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ API_ID = os.getenv('API_ID', 'your_api_id')
 API_HASH = os.getenv('API_HASH', 'your_api_hash')  
 BOT_TOKEN = os.getenv('BOT_TOKEN', 'your_bot_token')  
 MONGODB_URL = os.getenv('MONGODB_URL', 'your_mongodb_url')  # MongoDB URL
+BOT_IMAGE_URL = os.getenv('BOT_IMAGE_URL', 'your_default_image_url')  # Add this line
