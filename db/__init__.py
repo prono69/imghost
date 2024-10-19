6e76f2eb1b190b21b022baf3862bd13624b2f416
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-19 11:34:01 +0530

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,6 +1,6 @@
 # db/__init__.py
 
-from .mongo_db import mongo_db  # Import the mongo_db instance
+from .mongo_db import mongo_db, connect_to_mongodb
 
 def save_image_url(file_url):
     """Save an image URL to the database."""
