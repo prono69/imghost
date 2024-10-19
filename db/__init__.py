7a465e51dad44de19c1ab3cf48ba7392586ed85e
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-19 10:08:24 +0530

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,5 +1,5 @@
 # database/__init__.py
 
-from .mongodb import connect_to_mongodb, save_image_url  # Example functions
+from .mongodb import connect_to_mongodb, save_image_url  # Replace these with actual function names
 
 __all__ = ['connect_to_mongodb', 'save_image_url']  # Specifies what should be imported
