22603bb77c5f36ce9e4e936fa885de908c2a72f8
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-19 10:09:21 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,5 @@
+# handlers/__init__.py
+
+from .photo_handler import handle_photo  # Importing photo handling function
+
+__all__ = ['handle_photo']  # What can be imported with 'from handlers import *'
