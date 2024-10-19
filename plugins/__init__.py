b29b3470c931ace27a3dfc903671dc5b6a08f9cd
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-19 11:53:53 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,7 +3,7 @@
 from .start import start_handler       # Import start_handler from start.py
 from .help import help_handler         # Import help_handler from help.py
 from .stats import stats_command       # Import stats_handler from stats.py
-from .brodcast import broadcast_command  # Import broadcast_handler from brodcast.py
+from .broadcast import broadcast_command  # Import broadcast_handler from brodcast.py
 
 __all__ = [
     'start_handler', 
