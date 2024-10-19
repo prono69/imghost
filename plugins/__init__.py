a3f8a8c4770dadbe1602f2195aba9029040c1fe8
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-19 10:07:49 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,7 +1,6 @@
 # plugins/__init__.py
 
-from .start import start_handler
-from .help import help_handler
-# Import any other plugin handlers here if you have them
+from .start import start_handler  # Assuming you have a start_handler function in start.py
+from .help import help_handler    # Assuming you have a help_handler function in help.py
 
 __all__ = ['start_handler', 'help_handler']  # Specifies what should be imported when using 'from plugins import *'
