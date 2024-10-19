6f0b1eb1238cd1884c5e0e7bcaa63f89ebde4efa
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-19 10:33:13 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -7,5 +7,10 @@ async def help_command(client: Client, message):
         "Here are some commands you can use:\n"
         "/start - Start the bot\n"
         "Send me a photo to upload it.\n",
-        reply_markup={"inline_keyboard": [[("Return", "return")]]}
+        reply_markup={
+            "inline_keyboard": [
+                [("Return", "return")],
+                [("Close", "close")]
+            ]
+        }
     )
