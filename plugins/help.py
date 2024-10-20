9c67e3904e722923c2a42046b182cdef8f3a18df
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-20 08:19:55 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -2,16 +2,16 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.command("help"))
-async def command_help(client: Client, message):
+async def help_handler(client: Client, message):  # Change function name to help_handler
     # Create help menu buttons
     buttons = [
         [InlineKeyboardButton("Close", callback_data="close")]
     ]
-    
+
     reply_markup = InlineKeyboardMarkup(buttons)
 
     # Send help menu message
-    await message.reply(
+    help_message = await message.reply(
         "Here are some commands you can use:\n"
         "/start - Start the bot\n"
         "/help - Get help with commands\n"
@@ -20,3 +20,9 @@ async def command_help(client: Client, message):
         "Send me a photo to upload it and I'll provide a shareable link.\n",
         reply_markup=reply_markup
     )
+
+    # Listen for the callback query to close the help menu
+    @Client.on_callback_query(filters.regex("close"))
+    async def close_help(_, callback_query):
+        await callback_query.message.delete()  # Delete the help menu message
+        await callback_query.answer()  # Acknowledge the callback query
