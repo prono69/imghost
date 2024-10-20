3aa5698a5db8eea5b0b413f08f1e4190e4083376
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-20 08:05:51 +0530

    Update stats.py

--- plugins/stats.py
+++ plugins/stats.py
@@ -1,16 +1,14 @@
-# plugins/stats.py
-
 from pyrogram import Client, filters
 from config import ADMIN_ID
-from db.mongo_db import mongo_db  # Import your mongo_db instance for database access
+from db import mongo_db  # Import your mongo_db instance for database access
 
 async def stats_command(client: Client, message):
     if message.from_user.id == ADMIN_ID:
         try:
             # Fetch total users and uploads from the database
-            total_users = mongo_db.get_total_users()  # Should return the count of users
+            total_users = mongo_db.count_users()  # Should return the count of users
             total_uploads = mongo_db.count_uploads()  # Should return the count of uploads
-            
+
             await message.reply(
                 f"**Bot Statistics:**\n"
                 f"Total Users: {total_users}\n"
