d1fc3412bf07bdc83905fb38d42b0f59aa9ccbb7
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-20 17:49:13 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,7 +1,7 @@
-from .start import start_handler       # Import start_handler from start.py
+from .start import start_handler       
 from .help import help_handler   
-# Import help_handler from help.py
+
 __all__ = [
     'start_handler', 
-    'help_handler',  # Add all handlers to __all__
+    'help_handler',  
 ]
