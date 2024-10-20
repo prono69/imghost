832f3d3979972cac9f62d26bd45f5db507199c03
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2024-10-20 17:46:50 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,6 @@
-# handlers/photo_handler.py
-
-from db import save_image_url  # Importing the save_image_url function
+from db import save_image_url  
 
 async def handle_photo(client, message):
-    # Your photo handling logic
-    file_url = "example_file_url"  # Replace with actual file URL logic
-    save_image_url(file_url)  # Use save_image_url to insert the upload
+    
+    file_url = "example_file_url"  
+    save_image_url(file_url)  
