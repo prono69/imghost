8dca147d91f65caf4d631f8e0c4b5980a9099ad8
prono69 <faltu9851@gmail.com>
2024-12-07 22:58:51 +0530

    update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -3,6 +3,6 @@ from .mongo_db import mongo_db, connect_to_mongodb
 def save_image_url(file_url):
     """Save an image URL to the database."""
     try:
-        await mongo_db.insert_upload(file_url)
+        mongo_db.insert_upload(file_url)
     except Exception as e:
         print(f"Error saving image URL: {e}")
